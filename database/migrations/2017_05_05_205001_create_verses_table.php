4c020ccd70d163f0b56ecc390d928c3e1428c04d
Tomasz Budny <budny.tomasz@gmail.com>
2017-05-19 20:50:58 +0200

    Migrations - Books, Chapters, Verses + View

---
+++ database/migrations/2017_05_05_205001_create_verses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVersesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('verses', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->unsignedSmallInteger('verse_no');
+            $table->unsignedSmallInteger('chapter_id');
+            $table->text('body');
+            $table->timestamps();
+
+            $table->foreign('chapter_id')->references('id')->on('chapters')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('verses');
+    }
+}
---
+++ database/migrations/2017_05_06_182111_create_verses_chapters_books_view.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVersesChaptersBooksView extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement('
+            CREATE VIEW verses_chapters_books AS
+            SELECT 
+                v.id, b.id AS book_id, 
+                b.title AS book_title, 
+                c.chapter_no, 
+                c.title AS chapter_title, 
+                v.verse_no AS verse, 
+                v.body
+            FROM verses v
+            LEFT JOIN chapters c ON c.id = v.chapter_id
+            LEFT JOIN books b ON b.id = c.book_id
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('DROP VIEW `verses_chapters_books`');
+    }
+}
