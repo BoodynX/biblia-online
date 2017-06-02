13dc24bb201a6ce81390e740ef96091a27c313b9
Tomasz Budny <budny.tomasz@gmail.com>
2017-06-03 00:23:58 +0200

    WEB Routes - with Auth

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,26 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::pattern('book', '[0-9]+');
+Route::pattern('chapter', '[0-9]+');
+Route::pattern('verse', '[0-9]+');
+
+Route::get('/', function () {return view('welcome');});
+Route::get('/book/{book}/chapter/{chapter}/verse/{verse}', 'VersesController@show');
+Route::get('/book/{book}/chapter/{chapter}', 'ChaptersController@show');
+
+
+// @TODO The following controllers / methods need to be created
+
+Route::get('/book/{book}/chapters', 'ChaptersController@index');
+//Route::get('/book/{book}', 'BooksController@show');
+//Route::get('/books', 'BooksController@index');
+
+// Probably unnecessary for now
+// Route::get('/book/{book}/chapter/{chapter}/verses', 'VersesController@index');
+// Route::get('/book/{book}/chapter/{chapter}/verses/{from}/to/{to}', 'VersesController@set');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/test', 'TestController@index');
