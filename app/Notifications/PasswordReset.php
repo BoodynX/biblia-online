3620474d7d71a3f30425e5e33467e1a8ce297734
Tomasz Budny <budny.tomasz@gmail.com>
2017-06-04 17:15:40 +0200

    Custom Password Reset notification

---
+++ app/Notifications/PasswordReset.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class PasswordReset extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @param $token
+     * @return void
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->from('biblia@example.com', __('basic.bible'))
+            ->subject(__('auth.pass_reset'))
+            ->line(__('You are receiving this email because we received a password reset request for your account.'))
+            ->action(__('auth.pass_reset'), route('password.reset', $this->token))
+            ->line(__('If you did not request a password reset, no further action is required.'));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
