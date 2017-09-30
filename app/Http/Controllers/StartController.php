04213d86cd9324f052ba06e5cd32721d24d61ed1
Tomasz Budny <budny.tomasz@gmail.com>
2017-09-30 03:45:22 +0200

    Start elastic - dynamic books listing

--- app/Http/Controllers/StartController.php
+++ app/Http/Controllers/StartController.php
@@ -15,18 +15,21 @@ class StartController extends Controller
     public function index()
     {
         $user = Auth::user();
+        $books = array();
+        $chapters = array();
         $user_plan_days = UserPlanDay::where('user_id', $user->id)
-            ->with('userPlanSteps.planStep.chapter')->get();
+            ->with('userPlanSteps.planStep.chapter.book')->get();
 
         foreach ($user_plan_days as $user_plan_day) {
             $userPlanSteps = $user_plan_day->userPlanSteps;
             foreach ($userPlanSteps as $user_plan_step) {
                 $chapter = $user_plan_step->planStep->chapter;
                 $chapter->setStatus($user_plan_step->status);
+                $books[$chapter->book_id] = $chapter->book->title;
                 $chapters[$chapter->book_id][$chapter->id] = $chapter;
             }
         }
 
-        return view('start', compact('chapters'));
+        return view('start', compact('books', 'chapters'));
     }
 }
--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -4,39 +4,19 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Księga I</div>
-                <div class="panel-body">
-                    @foreach($chapters[1] as $c)
-                        <p>
-                            <a href="ksiega/{{$c->book_id}}/rozdzial/{{$c->chapter_no}}">Rozdział {{$c->chapter_no}} - {{$c->title}}</a>
-                            @if ($c->getStatus() == 'new') <span class="label label-info">Nowy</span> @endif
-                        </p>
-                    @endforeach
+            @foreach($books as $book_no => $book_title)
+                <div class="panel panel-default">
+                    <div class="panel-heading">Księga {{$book_no}} - {{$book_title}}</div>
+                    <div class="panel-body">
+                        @foreach($chapters[$book_no] as $c)
+                            <p>
+                                <a href="ksiega/{{$c->book_id}}/rozdzial/{{$c->chapter_no}}">Rozdział {{$c->chapter_no}} - {{$c->title}}</a>
+                                @if ($c->getStatus() == 'new') <span class="label label-info">Nowy</span> @endif
+                            </p>
+                        @endforeach
+                    </div>
                 </div>
-            </div>
-            <div class="panel panel-default">
-                <div class="panel-heading">Księga II</div>
-                <div class="panel-body">
-                    @foreach($chapters[2] as $c)
-                        <p>
-                            <a href="ksiega/{{$c->book_id}}/rozdzial/{{$c->chapter_no}}">Rozdział {{$c->chapter_no}} - {{$c->title}}</a>
-                            @if ($c->getStatus() == 'new') <span class="label label-info">Nowy</span> @endif
-                        </p>
-                    @endforeach
-                </div>
-            </div>
-            <div class="panel panel-default">
-                <div class="panel-heading">Księga III</div>
-                <div class="panel-body">
-                    @foreach($chapters[3] as $c)
-                        <p>
-                            <a href="ksiega/{{$c->book_id}}/rozdzial/{{$c->chapter_no}}">Rozdział {{$c->chapter_no}} - {{$c->title}}</a>
-                            @if ($c->getStatus() == 'new') <span class="label label-info">Nowy</span> @endif
-                        </p>
-                    @endforeach
-                </div>
-            </div>
+            @endforeach
         </div>
     </div>
 </div>
