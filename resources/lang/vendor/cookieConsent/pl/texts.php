ef2d777757dc2494455af32638cbe27459688022
Tomasz Budny <budny.tomasz@gmail.com>
2017-10-01 02:16:14 +0200

    Cookie Consent v0.1.1

---
+++ resources/lang/vendor/cookieConsent/pl/texts.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'message' => 'Ta strona używa ciasteczek (cookies), dzięki którym nasz serwis może działać lepiej.',
+    'agree' => 'OK',
+];
---
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -0,0 +1,12 @@
+
+<div class="js-cookie-consent cookie-consent">
+    <div class="alert alert-info" role="alert">
+        <span class="cookie-consent__message">
+            {!! trans('cookieConsent::texts.message') !!}
+        </span>
+
+        <button type="button" class="js-cookie-consent-agree cookie-consent__agree btn btn-primary" data-dismiss="alert">
+            {{ trans('cookieConsent::texts.agree') }}
+        </button>
+    </div>
+</div>
